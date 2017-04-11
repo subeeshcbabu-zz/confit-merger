1ef069782bbd5576f3562dc489aff7358478d18d
subeesh chothendavida <suchothendav@paypal.com>
2017-04-11 14:34:36 -0700

    unit test for protocols option

--- test/confit-merger.js
+++ test/confit-merger.js
@@ -1,6 +1,7 @@
 const Test = require('ava');
 const ConfitMerger = require('../index');
 const Path = require('path');
+const Handlers = require('shortstop-handlers');
 
 Test('basic merger', t => {
 
@@ -31,5 +32,23 @@ Test('Three level confit merger', t => {
         t.is(confit.get('b'), 'hi from default config');
         t.is(confit.get('c'), 'hi from module config');
         t.is(confit.get('d'), 'hi from app config');
+    });
+});
+
+Test('protocol option', t => {
+
+    process.env.MY_ENV = 'success';
+
+    return ConfitMerger( {
+        paths: [
+            Path.resolve(__dirname, 'fixtures', 'config'),
+            Path.resolve(__dirname, 'fixtures', 'moduleconfig')
+        ],
+        protocols: {
+            new: Handlers.env()
+        }
+    }).then(confit => {
+        t.truthy(confit);
+        t.is(confit.get('abc'), process.env.MY_ENV);
     });
 });
